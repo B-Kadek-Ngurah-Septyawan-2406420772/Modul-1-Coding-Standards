c74d4314ab4d305b75d588381618e733ec50aa20
cloudthegoat <awan.panjer@gmail.com>
2026-02-10 18:22:23 +0700

    feat(validation): add input validation for product name and quantity [FOR BONUS POINTS]

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -2,9 +2,11 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +26,10 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public String createProductPost (@ModelAttribute Product product, Model model) {
+    public String createProductPost (@Valid @ModelAttribute Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "createProduct";
+        }
         service.create(product);
         return "redirect:list";
     }
@@ -47,7 +52,10 @@ public class ProductController {
     }
 
     @PostMapping("/edit")
-    public String editProductPost (@ModelAttribute Product product, Model model) {
+    public String editProductPost (@Valid @ModelAttribute Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "editProduct";
+        }
         Product updatedProduct = service.update(product);
         if (updatedProduct == null) {
             return "redirect:list";
