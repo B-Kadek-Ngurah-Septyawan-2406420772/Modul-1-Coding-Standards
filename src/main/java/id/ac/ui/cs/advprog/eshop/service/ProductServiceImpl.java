cedea8447297d0d8765b5de802760819035a32c9
cloudthegoat <awan.panjer@gmail.com>
2026-03-03 18:57:29 +0700

    refactor(service): make services easier to test with constructor injection

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,33 +1,41 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import id.ac.ui.cs.advprog.eshop.repository.ProductReadRepository;
+import id.ac.ui.cs.advprog.eshop.repository.ProductWriteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductWriteRepository productWriteRepository;
+    private final ProductReadRepository productReadRepository;
+    private final ProductIdAssigner productIdAssigner;
+
+    public ProductServiceImpl(
+            ProductWriteRepository productWriteRepository,
+            ProductReadRepository productReadRepository,
+            ProductIdAssigner productIdAssigner
+    ) {
+        this.productWriteRepository = productWriteRepository;
+        this.productReadRepository = productReadRepository;
+        this.productIdAssigner = productIdAssigner;
+    }
 
     @Override
     public Product create(Product product) {
-        if (product.getProductId() == null || product.getProductId().isBlank()) {
-            product.setProductId(UUID.randomUUID().toString());
-        }
-        productRepository.create(product);
+        product.setProductId(productIdAssigner.assign(product));
+        productWriteRepository.create(product);
         return product;
     }
 
     @Override
     public List<Product> findAll() {
-        Iterator<Product> productIterator = productRepository.findAll();
+        Iterator<Product> productIterator = productReadRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining (allProduct::add);
         return allProduct;
@@ -35,16 +43,16 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product findById(String productId) {
-        return productRepository.findById(productId);
+        return productReadRepository.findById(productId);
     }
 
     @Override
     public Product update(Product product) {
-        return productRepository.update(product);
+        return productWriteRepository.update(product);
     }
 
     @Override
     public Product delete(String productId) {
-        return productRepository.delete(productId);
+        return productWriteRepository.delete(productId);
     }
 }
